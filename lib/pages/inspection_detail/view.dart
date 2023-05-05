a66cd954acf31aa15fc875d888c2b0a3650d357c
Estim <2423860442@qq.com>
2023-05-05 21:18:35 +0800

    1

--- lib/pages/inspection_detail/view.dart
+++ lib/pages/inspection_detail/view.dart
@@ -31,115 +31,132 @@ class InspectionDetailPage extends GetView<InspectionDetailController> {
         title:  Text("巡检详情",style: Styles.headLineStyle3.copyWith(fontSize: 22.sp,color: Colors.white),),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("设备名称",style: Styles.headLineStyle4.copyWith(color: Colors.grey,fontSize: 16.sp),),
-
-                Text(controller.item.equipmentName,style: Styles.headLineStyle4.copyWith(color: Colors.black,fontSize: 16.sp),),
-              ],
-            ),
-            SizedBox(height: 15.h,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("设备地址",style: Styles.headLineStyle4.copyWith(color: Colors.grey,fontSize: 16.sp),),
-                Text(controller.item.address,style: Styles.headLineStyle4.copyWith(color: Colors.black,fontSize: 16.sp),),
-              ],
-            ),
-            Row(
-              children: [
-                Text(
-                  "检查项目",
-                  style:
-                      TextStyle(fontSize: 16.sp, fontWeight: FontWeight.bold),
-                ),
-                Expanded(child: Container()),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 8.r, right: 8.r, left: 8.r),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Table(
-                    border: const TableBorder(
-                      top: BorderSide(
-                          color: Color(0xFF000000),
-                          width: 1,
-                          style: BorderStyle.solid),
-                      // horizontalInside: BorderSide(width: 1, color: Colors.grey),
-                      bottom: BorderSide.none,
-                      left: BorderSide(
-                          color: Color(0xFF000000),
-                          width: 1,
-                          style: BorderStyle.solid),
-                      right: BorderSide(
-                          color: Color(0xFF000000),
-                          width: 1,
-                          style: BorderStyle.solid),
-                      verticalInside: BorderSide(
-                          color: Color(0xFF000000),
-                          width: 1,
-                          style: BorderStyle.solid),
-                    ),
-                    // border: TableBorder.all(), // 设置表格边框
-                    children: [
-                      TableRow(
-                        children: [
-                          TableCell(
-                            child: Container(
-                                height: 50,
-                                alignment: Alignment.center,
-                                child: Text('巡检点')),
-                          ),
-                          TableCell(
-                            child: Container(
-                                height: 50,
-                                alignment: Alignment.center,
-                                child: Text('信息')),
-                          ),
-                          TableCell(
-                            child: Container(
-                                height: 50,
-                                alignment: Alignment.center,
-                                child: Text('结论')),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
+                  Text("设备名称",style: Styles.headLineStyle4.copyWith(color: Colors.grey,fontSize: 16.sp),),
+
+                  Text(controller.item.equipmentName,style: Styles.headLineStyle4.copyWith(color: Colors.black,fontSize: 16.sp),),
                 ],
               ),
-            ),
-            ...controller.inspectionTaskFormList,
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: EchoAssetPicker(
-                maxAssets: 9,
-                bgColor: Colors.transparent,
-                oldAssets: controller.assets,
-                callBack: (assetList) {
-                  controller.assets = assetList;
-                },
+              SizedBox(height: 15.h,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("设备地址",style: Styles.headLineStyle4.copyWith(color: Colors.grey,fontSize: 16.sp),),
+                  Text(controller.item.address,style: Styles.headLineStyle4.copyWith(color: Colors.black,fontSize: 16.sp),),
+                ],
               ),
-            ),
-            SizedBox(
-              height: 30.h,
-            ),
-            EchoButton(
-                height: 40.h,
-                buttonName: "完成",
-                buttonColor: AppColors.primary,
-                buttonTextStyle: TextStyle(
-                  fontSize: 16.sp
+              SizedBox(height: 15.h,),
+              Row(
+                children: [
+                  Text(
+                    "检查项目",
+                    style:
+                        TextStyle(fontSize: 18.sp, fontWeight: FontWeight.bold,color: AppColors.primary),
+                  ),
+                  Expanded(child: Container()),
+                ],
+              ),
+              SizedBox(height: 15.h,),
+              Padding(
+                padding: EdgeInsets.only(top: 8.r, right: 8.r, left: 8.r),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Table(
+                      border: const TableBorder(
+                        top: BorderSide(
+                            color: Color(0xFF000000),
+                            width: 1,
+                            style: BorderStyle.solid),
+                        // horizontalInside: BorderSide(width: 1, color: Colors.grey),
+                        bottom: BorderSide.none,
+                        left: BorderSide(
+                            color: Color(0xFF000000),
+                            width: 1,
+                            style: BorderStyle.solid),
+                        right: BorderSide(
+                            color: Color(0xFF000000),
+                            width: 1,
+                            style: BorderStyle.solid),
+                        verticalInside: BorderSide(
+                            color: Color(0xFF000000),
+                            width: 1,
+                            style: BorderStyle.solid),
+                      ),
+                      // border: TableBorder.all(), // 设置表格边框
+                      children: [
+                        TableRow(
+                          children: [
+                            TableCell(
+                              child: Container(
+                                  height: 50,
+                                  alignment: Alignment.center,
+                                  child: Text('巡检点')),
+                            ),
+                            TableCell(
+                              child: Container(
+                                  height: 50,
+                                  alignment: Alignment.center,
+                                  child: Text('信息')),
+                            ),
+                            TableCell(
+                              child: Container(
+                                  height: 50,
+                                  alignment: Alignment.center,
+                                  child: Text('结论')),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-                onPressed: () async{
-                  await controller.commit();
-                })
-          ],
+              ),
+              ...controller.inspectionTaskFormList,
+              SizedBox(height: 15.h,),
+              Row(
+                children: [
+                  Text(
+                    "附件",
+                    style:
+                    TextStyle(fontSize: 18.sp, fontWeight: FontWeight.bold,color: AppColors.primary),
+                  ),
+                  Expanded(child: Container()),
+                ],
+              ),
+              SizedBox(height: 15.h,),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: EchoAssetPicker(
+                  maxAssets: 9,
+                  bgColor: Colors.transparent,
+                  oldAssets: controller.assets,
+                  callBack: (assetList) {
+                    controller.assets = assetList;
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 30.h,
+              ),
+              EchoButton(
+                  height: 40.h,
+                  buttonName: "完成",
+                  buttonColor: AppColors.primary,
+                  buttonTextStyle: TextStyle(
+                    fontSize: 16.sp
+                  ),
+                  onPressed: () async{
+                    await controller.commit();
+                  })
+            ],
+          ),
         ),
       ),
     );
